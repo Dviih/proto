6630f415df82063d04bed092539bc18e15c4ed3c
Dviih <dviih2s@gmail.com>
2025-01-04 21:26:58 -0300

    Old stuff for Running and Value.

--- event/event.go
+++ event/event.go
@@ -68,13 +68,6 @@ func (event *Event) Unsubscribe(name string) {
 	fn.(js.Func).Release()
 }
 
-func (event *Event) Running() bool {
-	return event.running.Load()
-}
-
-func (event *Event) Value() js.Value {
-	if event.value.IsUndefined() {
-		event.forceValue()
 	}
 
 	return event.value
