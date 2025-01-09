0a9781946e0c9dd39137e3132cb70c7de1bd45f4
Dviih <dviih2s@gmail.com>
2025-01-08 21:16:50 -0300

    Get the js.Func not js.Value.

--- event/event.go
+++ event/event.go
@@ -47,12 +47,12 @@ func (event *Event) Running() bool {
 }
 
 func (event *Event) Subscribe(name string, fn func(js.Value, []js.Value) interface{}) {
-	_, err := event.events.LoadOrStore(name, js.FuncOf(fn))
+	jsf, err := event.events.LoadOrStore(name, js.FuncOf(fn))
 	if err != nil {
 		event.Unsubscribe(name)
 	}
 
-	event.value.Value().Call("addEventListener", name, fn)
+	event.value.Value().Call("addEventListener", name, jsf)
 }
 
 func (event *Event) Unsubscribe(name string) {
